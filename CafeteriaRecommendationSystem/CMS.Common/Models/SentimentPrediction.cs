0f8028cfc5682ed9566d96f3be8834b3f72222b4
Radhika Ojha <radhika.ojha@intimetec.com>
2024-07-03 22:16:20 +0530

    removed unused models

--- CafeteriaRecommendationSystem/CMS.Common/Models/SentimentPrediction.cs
+++ CafeteriaRecommendationSystem/CMS.Common/Models/SentimentPrediction.cs
@@ -7,13 +7,6 @@ using System.Threading.Tasks;
 
 namespace CMS.Common.Models
 {
-    public class CafeReview
-    {
-        public string FoodItem { get; set; }
-        public string Comment { get; set; }
-        public float SentimentScore { get; set; }
-        public bool IsPositive { get; set; }
-    }
     public class FoodReview
     {
         [LoadColumn(0)]
@@ -25,13 +18,6 @@ namespace CMS.Common.Models
         [LoadColumn(2)]
         public bool Sentiment { get; set; }
 
-    }
-    public class AnalyzedReview
-    {
-        public string FoodItem { get; set; }
-        public string ReviewText { get; set; }
-        public float SentimentScore { get; set; }
-        public List<string> KeyPhrases { get; set; }
     }
     public class SentimentPrediction
     {
